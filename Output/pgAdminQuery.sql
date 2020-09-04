9ccb3c6a63e849c4ed91b895f03141352f621467
Darryl Baynes <dbaynes9116@gmail.com>
2020-09-03 20:35:47 -0400

    added SQL that inner creates a list of actors and their movies

--- Output/pgAdminQuery.sql
+++ Output/pgAdminQuery.sql
@@ -1,12 +1,38 @@
-drop table actor_id;
+--drop table actor_id_df;
 
--- 1. Create a departments table
-CREATE TABLE actor_id (
-    entity varchar(30),
-	"ID Number" varchar(10),
-	primary key("ID Number")
+--drop table actor_actress_df;
+
+--drop table actor_id_title;
+
+
+
+CREATE TABLE actor_id_df(
+			entity VARCHAR (255),
+			"ID Number" INT
 );
 
---COPY actor_id from 'C:\Users\dbayn\Documents\GT Bootcamp\Projects\ETL-Project\Output\actor_id.csv' delimiter ',' csv header;
+CREATE TABLE actor_actress_df(
+			year VARCHAR(10),
+			category VARCHAR(50),
+			winner BOOLEAN,
+			entity VARCHAR(255),
+			"First Name" VARCHAR (50),
+			"Last Name" VARCHAR (50),
+			"ID Number" INT
+);
 
-select * from actor_id
+CREATE TABLE actor_id_title(
+	"Actor ID" INT,
+	"Movie Title" VARCHAR(255)
+);
+
+select * from actor_id_title;
+
+select * from actor_id_df;
+
+select * from actor_actress_df;
+
+select entity, "Movie Title"
+from actor_id_df
+join actor_id_title on actor_id_df."ID Number"=actor_id_title."Actor ID";
+
